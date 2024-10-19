a87a879f76b21b22392c47536435c55147dea304
sadow <sadowskimaciej226@gmail.com>
2024-10-19 18:28:26 +0200

    moved file to higher directory

--- src/integration/java/pl/joble/feature/HappyPathTestIntegration.java
+++ src/integration/java/pl/joble/feature/HappyPathTestIntegration.java
@@ -9,7 +9,6 @@ public class HappyPathTestIntegration extends BaseIntegrationTest {
     public void should_user_get_offers(){
 //        1.	Scheduler runs for the 1st time and made GET to server to get All joboffers
 //        2.	Scheduler made Get to server every 3 hours
-//        3.	User sign in by using POST /singin and inputs email name lastname age and password
 //        4.	System validates data and return 200 OK with JWT token if validation is successful or 400 BAD REQUEST if for example there is already user with that email
 //        5.	If account already exists user can POST /login with email and password to get jwt token, system can return 200OK with JWT or 400 BAD REQUEST if password is wrong
 //        6.	User with JWT(and in any endpoint except /login and /singin) can GET /joboffers,
