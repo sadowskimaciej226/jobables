b5d359a3b3172623e6dc801c856809d6aafcf4bf
sadow <sadowskimaciej226@gmail.com>
2024-10-19 18:29:58 +0200

    changed db name

--- init-mongo.js
+++ init-mongo.js
@@ -5,7 +5,7 @@ db.createUser(
         roles: [
             {
                 role: "readWrite",
-                db: "lotto-web"
+                db: "jobable-web"
             }
         ]
     }
