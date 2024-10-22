9d1a361b3ab0c587c65551632638284fbc3e6103
sadow <sadowskimaciej226@gmail.com>
2024-10-22 19:40:22 +0200

    deleted unnecessary import

--- src/main/java/pl/joble/domain/offer/dto/JobOfferDto.java
+++ src/main/java/pl/joble/domain/offer/dto/JobOfferDto.java
@@ -2,7 +2,6 @@ package pl.joble.domain.offer.dto;
 
 import lombok.Builder;
 
-import java.time.LocalDateTime;
 
 @Builder
 public record JobOfferDto(String id, String title, String description, String companyName,
