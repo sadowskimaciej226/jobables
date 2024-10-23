fa82c66952ce93c13739bbe07cd24cc821dbd5cf
sadow <sadowskimaciej226@gmail.com>
2024-10-23 13:37:29 +0200

    implemeted scheduler tests

---
+++ src/integration/java/pl/joble/feature/SampleJobOfferResponse.java
@@ -0,0 +1,22 @@
+package pl.joble.feature;
+
+public interface SampleJobOfferResponse {
+
+    default String bodyWithTwoOffersJson(){
+        return """
+                "title": "Junior Java Developer",
+                        "company": "BlueSoft Sp. z o.o.",
+                        "salary": "7 000 – 9 000 PLN",
+                        "offerUrl": "https://nofluffjobs.com/pl/job/junior-java-developer-bluesoft-remote-hfuanrre"
+                    },
+                    {
+                        "title": "Java (CMS) Developer",
+                        "company": "Efigence SA",
+                        "salary": "16 000 – 18 000 PLN",
+                        "offerUrl": "https://nofluffjobs.com/pl/job/java-cms-developer-efigence-warszawa-b4qs8loh"
+                """.trim();
+    }
+    default String bodyWithZeroOffersJson(){
+        return "[]";
+    }
+}
